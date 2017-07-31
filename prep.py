ab65d478daba6c41f10243bb87e337a557c4e778
duc linh <haixoay96@gmail.com>
2017-07-31 17:06:56 +0700

    fix pre

--- prep.py
+++ prep.py
@@ -8,9 +8,9 @@ import shutil
 import sys
 
 
-WIDTH=36
-HEIGHT=18
-COUNTRY='us'
+WIDTH=14
+HEIGHT=19
+COUNTRY='vnmoto'
 
 #WIDTH=52
 #HEIGHT=13
@@ -21,12 +21,12 @@ COUNTRY='us'
 #COUNTRY='br'
 
 #constants
-OPENCV_DIR= '/home/mhill/projects/alpr/libraries/opencv/bin'
+OPENCV_DIR= '/usr/bin'
 SAMPLE_CREATOR = OPENCV_DIR + '/opencv_createsamples'
 
-BASE_DIR            = '/home/mhill/projects/alpr/samples/training/'
+BASE_DIR            = '/home/duclinh/train-detector/'
 
-OUTPUT_DIR          = BASE_DIR + "out/"
+OUTPUT_DIR          = BASE_DIR + "out"
 INPUT_NEGATIVE_DIR  = BASE_DIR + 'raw-neg/'
 INPUT_POSITIVE_DIR  = BASE_DIR + COUNTRY + '/'
 OUTPUT_NEGATIVE_DIR  = BASE_DIR + 'negative/'
